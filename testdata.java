922703434ddab5a33b2413d98e78c19903203b46
anirudhau <129376772+anirudhau@users.noreply.github.com>
2023-03-30 18:55:51 +0700

    test

--- testdata.java
+++ testdata.java
@@ -1,5 +1,20 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String sayHello() {
+		return "Hello World!this is test code1333 created by Ani- chalo india-------------";
+	}
 }
